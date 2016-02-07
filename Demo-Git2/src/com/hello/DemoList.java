0a08b8aa272a927a6da0e2b214588c292e99a512
kalyan <kalyanrajput09@gmail.com>
2016-02-07 07:40:11 +0530

    ok

---
+++ Demo-Git2/src/com/hello/DemoList.java
@@ -0,0 +1,34 @@
+package com.hello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoList {
+
+	static List<String> keyfileList = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		for (int i = 0; i < keyfileList.size(); i++) {
+			// out outTable
+			// sum
+			// in inTable
+			i++;
+			// outTAble
+			// count
+			// inTable
+		}
+
+	}
+
+	public String outTable(int index) {
+		List<String> outkyeList = new ArrayList<String>();
+		String key = outkyeList.get(index);
+		return key;
+	}
+
+	public String inTable(int index) {
+		List<String> inkyeList = new ArrayList<String>();
+		String key = inkyeList.get(index);
+		return key;
+	}
+}
