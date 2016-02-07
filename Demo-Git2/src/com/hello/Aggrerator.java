976070ecd95629137f8ccb6e6b745be0b6f60d4b
kalyan <kalyanrajput09@gmail.com>
2016-02-07 22:22:11 +0530

    Change-Id: I9fe67c493f56cd1994ac04f9e70a27d7dc1265e7

--- Demo-Git2/src/com/hello/Aggrerator.java
+++ Demo-Git2/src/com/hello/Aggrerator.java
@@ -1,10 +1,27 @@
 package com.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aggrerator {
 
 	public static void main(String[] args) {
-		
+		List<String> keylist = new ArrayList<String>();
+		keylist.add("dep_id");
 
+		List<String> toatalListforout = new ArrayList<String>();
+		toatalListforout.add("dep_id");
+		toatalListforout.add("salary");
+		toatalListforout.add("sum_salary");
+
+		List<String> toatalListforIn = new ArrayList<String>();
+		toatalListforIn.add("dep_id");
+		toatalListforIn.add("count_dept");
+		System.out.println("");
+		
+		
 	}
+	
+	
 
 }
